5564d0656fe242f104296e2b5f45d6252949872f
Jogeonsang <rhcn258@gmail.com>
2018-08-22 16:53:32 +0900

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = ({match}) => {
+    return(
+        <div>
+            <h2>
+                About {match.params.name}
+            </h2>
+        </div>
+    );
+};
+
+export default About;
